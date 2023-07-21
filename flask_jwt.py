88b4f2fff6c8f39fdc4dff5589d50971cbb3801b
truee <zchuyi10@gmail.com>
2023-07-20 17:38:41 -0700

    implements jwt

---
+++ flask_jwt.py
@@ -0,0 +1,42 @@
+from flask import Flask
+from flask import request
+from datetime import datetime, timedelta
+import jwt,logging
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+my_secret = "my_secret_key"
+
+def create_token(user):
+    expire_time_datetime = datetime.now() + timedelta(hours=1)
+    payload ={
+        "user": user,
+        "expire-timestamp": expire_time_datetime.strftime("%m%d%Y,%H:%M:%S")
+    }
+    token = jwt.encode(payload=payload, key=my_secret)
+    return token
+
+
+@app.route('/')
+def hi():
+    return "hellos"
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+#        user = request.form.get['user']
+#        password = request.form.get['password']
+        app.logger.debug(request.json.get('user'))
+        user = request.json.get('user')
+        password = request.json.get('password')
+
+        
+        if user == "Mike" and password == "123":
+            token = create_token(user)
+            return {"result":"OK","token":token}
+        else:
+            return {"result":"fail", "reason":"password is wrong"}
+        
+if __name__ == '__main__':
+    app.run(debug=True)
